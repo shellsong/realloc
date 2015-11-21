ec5c26c1e0b20823e632a0cd58d88e078a248ead
foomorrow <stormsong3@gmail.com>
2015-11-21 22:24:36 +0800

    add travis

--- src/actionCreatorFactory.js
+++ src/actionCreatorFactory.js
@@ -1,32 +1,51 @@
+import { clone } from './utils'
 import Compiler, { parseJSONPath } from './JSONPathCompiler'
-import { createGetter, createSetter, evalJSONPath } from './createJSONPathAccessor'
 
 export default function actionCreatorFactory(stateParent, broadcastMap, collect){
   return (keyPath, fn, opts) => {
     const options = Object.assign({deps:[]}, opts)
-    const parsedKeyPath = parseJSONPath(keyPath).join('')
-    const get = createGetter(keyPath)
-    const set = createSetter(keyPath)
-    const depsGetters = options.deps.map(evalJSONPath)
+    const parsedKeyPath = parseJSONPath(keyPath)
+    const parsedKeyPathStr = parsedKeyPath.join('')
+    const compiler = new Compiler(parsedKeyPath, false)
+    const matcher = compiler.createMatcher()
+    const depsGetters = options.deps.map((kp) => (new Compiler(kp)).createMatcher())
     const castFns = Object.keys(broadcastMap)
-                           .filter((v) => parsedKeyPath.indexOf(v) > -1)
+                           .filter((v) => parsedKeyPathStr.indexOf(v) === 0)
                            .map((k) => broadcastMap[k])
                            .reduce((acc,i) => acc.concat(i), [])
     return (...payloads) => {
-      return fn.apply(
-        null,
-        payloads.concat(
-          depsGetters.map((get) => rs(stateParent.$, payloads))
-        ).concat(
-          [
-            getter(stateParent.$, payloads),
-            (newValue) => {
-              collect(set(stateParent, newValue, payloads))
-              castFns.forEach((fn) => fn.apply(null, payloads))
-            }
-          ]
+      return matcher(stateParent.$, payloads).map((result) => {
+        return fn.apply(
+          null,
+          payloads.concat(
+            depsGetters.map((get) => get(stateParent.$, payloads).map((r) => r.value))
+          ).concat(
+            [
+              result.value,
+              (newValue) => {
+                var $ = clone(stateParent.$)
+                  , newCur = $
+                  , oldCur = stateParent.$
+                  , pwd
+                if(result.pwd[0] === '$'){
+                  pwd = result.pwd.slice(1)
+                }else{
+                  pwd = result.pwd
+                }
+                pwd.forEach((key) => {
+                  newCur[key] = clone(oldCur[key])
+                  newCur = newCur[key]
+                  oldCur = oldCur[key]
+                })
+                newCur[result.name] = newValue
+                collect($)
+                castFns.forEach((fn) => fn.apply(null, payloads))
+              },
+              result
+            ]
+          )
         )
-      )
+      })
     }
   }
 
